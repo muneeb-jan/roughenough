9ffefe470e40fc3ed717dec91e3e9e9ade692828
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2022-09-22 21:45:07 +0200

    Take midpoint instead of rounded down val for mean

--- parser.py
+++ parser.py
@@ -7,8 +7,9 @@ lines = f.readlines()
 count=0
 tsum=0
 for line in lines:
-    client_ms = int(line.split(" ")[1].split(',')[1])
-    server_ms = int(line.split(" ")[12].split('.')[1])//1000000
+    client_ms = int(line.split(" ")[1].split(',')[1])*1000+500
+    server_ms = int(line.split(" ")[12].split('.')[1])//1000
     count += 1
     tsum += (client_ms - server_ms)
-print("Count: ", count, "\nAverage: ", tsum/count)
+    print(client_ms, server_ms)
+print("Count: ", count, "\nMean Error: ", tsum/count, "micros")
